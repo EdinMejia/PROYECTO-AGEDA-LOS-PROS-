9198a77af16f90291ec3eca5f5ea2c58cee6130e
Osvado100 <cuaosvaldo@gmail.com>
2025-06-04 20:23:10 -0600

    avance osvaldo miercoles

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -1,3 +1,13 @@
 import java.io.*; 
 import java.util.*;
 
+
+public class AgendaTelefonica {
+
+    private string nombres;
+    private string correo;
+    private string direccion;
+    private int telefono;
+
+    
+}
