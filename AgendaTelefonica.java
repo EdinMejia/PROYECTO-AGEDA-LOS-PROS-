c9f3e88e2f4b652c9e07950599b070963a1eb5c7
Osvado100 <cuaosvaldo@gmail.com>
2025-06-04 22:17:29 -0600

    Update AgendaTelefonica.java

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -8,6 +8,14 @@ public class AgendaTelefonica {
     private string correo;
     private string direccion;
     private int telefono;
-
     
-}
+
+    public AgendaTelefonica (string nombres, string correo, string direccion, int telefono) {
+        this.nombres = nombres;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.telefono = telefono;
+
+
+
+    }
