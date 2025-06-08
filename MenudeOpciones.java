a8c2a7988fa3725b9201f6af9c9101489fc520d5
Edin Mejía <edinmejia1208@gmail.com>
2025-06-08 09:36:56 -0600

    Interface 4

--- MenudeOpciones.java
+++ MenudeOpciones.java
@@ -16,6 +16,7 @@ public class MenudeOpciones {
             System.out.println("\n 5- Eliminar contacto ");
             System.out.println("\n 6- Exportar a .txt ");
             System.out.println("\n 7- Salir ");
+            System.out.println("\n 8. Mostrar contactos ordenados por nombre: ");
             System.out.println("\n Seleccione opcion:  ");
             int opcion = sc.nextInt(); sc.nextInt();
             
@@ -29,7 +30,7 @@ public class MenudeOpciones {
                     String direc = sc.nextLine();
                     System.out.println("Telefono");
                     String tel = sc.nextLine();
-                    agenda.agregar(new Contacto(nombre, mail, direc,tel));
+                    agenda.agregar(new Contacto(nombre, mail, direc, tel));
                     break;
                 case 2: 
                     System.out.println("Ingrese Nombre: ");
@@ -45,8 +46,40 @@ public class MenudeOpciones {
                         System.out.println(c2.getNombre() + " - " + c2.getTelefono() + " - " + c2.getCorreo());
                     else System.out.println("no encontrado: "); 
                     break; 
-                default:
+                case 4: 
+                    System.out.println("Nombre del contacto a Editar: ");
+                    Contacto c3 = agenda.buscarPorNombre(sc.nextLine()); 
+                    if (c3 != null) {
+                        System.out.println("Nuevo nombre: ");
+                        c3.setNombre(sc.nextLine()); 
+                        System.out.println("nuevo correo");
+                        c3.setCorreo(sc.nextLine()); 
+                        System.out.println("nueva direccion");
+                        c3.setDireccion(sc.nextLine()); 
+                        System.out.println("nuevo telefono");
+                        c3.setTelefono(sc.nextLine()); 
+                    }else {
+                        System.out.println("contacto no encontrado: ");
+                    }
+                    break; 
+                case 5: 
+                    System.out.print( "Nombre del contacto a eliminar: ");
+                    if (agenda.eliminar(sc.nextLine()))
+                    System.out.println("Eliminado con exito. ");
+                    else System.out.println("No se encontro el contacto: ");
+                    break; 
+                case 6: 
+                    Exportador.exportarAArchivo(agenda.obtenerTodos(), nombreArchivo:"contactos.txt"); 
                     break;
+                case 7:
+                    System.out.println("Saliendo de agenda........");
+                    return; 
+                case 8: 
+                    System.out.println("Contactos ordenados alfabeticamente: ");
+                    agenda.mostrarOrdenadoPorNombre();
+                    break; 
+                default:
+                    System.out.println("Opcion no valida. ");
             }
             
         }
