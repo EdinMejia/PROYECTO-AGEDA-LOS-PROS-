a244c2e133910cfa39dc703f034e6ab4431621de
JoseDanielPu <pudany86@gmail.com>
2025-06-09 11:31:13 -0600

    clase exportador a txt
    espero que funcione porque no tengo para el cifrado cesar pero me avisan

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -1,3 +1,17 @@
 import java.io.*; 
 import java.util.*;
-
+// clase exportador a txt 
+public class Exportador {
+    public static void exportar(ListaContactos lista, String nombreArchivo) {
+        try (FileWriter writer = new FileWriter(nombreArchivo)) {
+            Nodo actual = lista.getCabeza();
+            while (actual != null) {
+                writer.write(actual.contacto.toString() + "\n------------------------\n");
+                actual = actual.siguiente;
+            }
+            System.out.println("Contactos exportados correctamente a " + nombreArchivo);
+        } catch (IOException e) {
+            System.out.println("Error al exportar contactos: " + e.getMessage());
+        }
+    }
+}
