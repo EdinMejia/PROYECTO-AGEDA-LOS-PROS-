643c4a43d580860634b37860650e84b1f4e0b591
JoseDanielPu <pudany86@gmail.com>
2025-06-10 12:40:28 -0600

    actualización del código
    integración de los codificadores de nombre telefóno correo y dirección

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -1,17 +1,20 @@
 import java.io.*; 
 import java.util.*;
 // clase exportador a txt 
-public class Exportador {
-    public static void exportar(ListaContactos lista, String nombreArchivo) {
-        try (FileWriter writer = new FileWriter(nombreArchivo)) {
-            Nodo actual = lista.getCabeza();
-            while (actual != null) {
-                writer.write(actual.contacto.toString() + "\n------------------------\n");
-                actual = actual.siguiente;
+class Exportador { // Declaramos la clase llamada Exportador
+        public static void exportarAArchivo(List<Contacto> contactos, String nombreArchivo) {
+            try { // creamos el bloque de codigo 
+                FileWriter fw = new FileWriter(nombreArchivo); // esto crea un objeto FileWriter que creara el archivo .txt
+                for (Contacto c : contactos) {
+                    fw.write("Nombre: " + c.getNombre() +
+                            ", Teléfono: " + c.getTelefono() +
+                            ", Correo: " + c.getCorreo() +
+                            ", Dirección: " + c.getDireccion() + "\n");
+                }
+                fw.close(); // cerramos el archibo 
+                System.out.println("Exportación exitosa a " + nombreArchivo);
+            } catch (IOException e) { // si no se culple el bloque anterior se ejecuta este 
+                System.out.println("Error al exportar: " + e.getMessage());
             }
-            System.out.println("Contactos exportados correctamente a " + nombreArchivo);
-        } catch (IOException e) {
-            System.out.println("Error al exportar contactos: " + e.getMessage());
         }
     }
-}
