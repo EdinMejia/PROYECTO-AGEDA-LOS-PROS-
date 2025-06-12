6758e57843a2782023c733fb2cfd79276d4cf798
Osvado100 <cuaosvaldo@gmail.com>
2025-06-11 21:36:48 -0600

    AVANCE
    AVANCE MIERCOLES CLASE CONTACTOS

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -1,35 +1,36 @@
-import java.io.*; 
-import java.util.*;
+// CLASE Contacto
+class Contacto {
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String direccion;
 
+    public Contacto(String nombre, String telefono, String correo, String direccion) {
+        this.nombre = nombre;
+        this.telefono = Cifrado.cifrarCesar(telefono, 3);
+        this.correo = Cifrado.cifrarCesar(correo, 3);
+        this.direccion = direccion;
 
-public class AgendaTelefonica {
-
-    private string nombres;
-    private string correo;
-    private string direccion;
-    private int telefono;
-    
-
-    public AgendaTelefonica (string nombres, string correo, string direccion, int telefono) {
-        this.nombres = nombres;
-        this.correo = cifrado.cifrardocesar(telefono, desp:3);
-        this.direccion = cifrado.cifrarcesar(direccion, desp:3);
-        this.telefono = cifrado.cifrarcesar(telefono; desp:3)
-
-        system.out.println(x:"contacto agregado:");
-        system.out.println(" telefono original:" + telefono +"cifrado:" this.telefono);
-        system.out.println("correo original:" + correo+ "cifrado" + this.correo);
-        system.out.println("direccion:" + "cifrado" + this.direccion);
-
-        public string getnombre() {return nombre};
-        public string getcorreo() {return cifrado. descifrarcesar (telefono, desp: 3); }
-        public string getdireccion() {return cifrado. descifrarcesar (direccion, desp: 3); }
-        public int gettelefono() {return cifrado. descifrarcesar (telefono, desp: 3): }
-
-        public void setnombre(string nombre) {this,nombre = cifrado.cifradocesar (nombre, desp: 3); } 
-        public void setcorreo(string correo) {this,correo = cifrado.cifradocesar (correo, desp: 3); }
-        public void setdireccion(string direccion) {this,direccion = cifrado.cifradocesar (direccion, desp: 3); }
-        public void settelefono(string telefono) {this,telefono = cifrado.cifradocesar (telefono, desp: 3); }
-
-
+        System.out.println("\n>> Contacto agregado:");
+        System.out.println("Teléfono original : " + telefono + " -> cifrado: " + this.telefono);
+        System.out.println("Correo original   : " + correo + " -> cifrado: " + this.correo);
+        System.out.println("Dirección         : " + this.direccion);
     }
+
+    public String getNombre() { return nombre; }
+    public String getTelefono() { return Cifrado.descifrarCesar(telefono, 3); }
+    public String getCorreo() { return Cifrado.descifrarCesar(correo, 3); }
+    public String getDireccion() { return direccion; }
+
+    public void setTelefono(String telefono) {
+        this.telefono = Cifrado.cifrarCesar(telefono, 3);
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = Cifrado.cifrarCesar(correo, 3);
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
